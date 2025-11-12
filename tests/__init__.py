ce5d00d64af22072512d66de0859bc915b5d0339
GPrabhanjana <ghurikiprabhanjana@gmail.com>
2025-11-12 22:55:13 +0530

    Initial commit: setup VizGrad package

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+"""
+Test suite for VizGrad
+
+This package contains unit tests for all VizGrad functionality including:
+- Arithmetic operations
+- Activation functions
+- Matrix operations
+- Gradient computation
+- Visualization utilities
+"""
